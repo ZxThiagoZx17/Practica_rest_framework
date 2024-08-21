04f791675755f11e599fc4aa8665ac0c7fec7318
unknown <tiagosanabria03@gmail.com>
2024-08-20 20:43:33 -0500

    Se configura archivo urls.py a nivel de proyecto, se usa router de la mano de viewset para definir las rutas

--- practica_rest_framework/urls.py
+++ practica_rest_framework/urls.py
@@ -15,8 +15,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
+#Definen y organizan rutas de las aplicaciones
+from django.urls import path, include
+
+#Generean automaticamente las rutas de los viewsets
+from rest_framework import routers
+
+#Importamos vistas creadas previamente
+from API import views
+
+#Creamos un router para el manejo de rutas para los viewsets, tambien nos da un GUI para navegar en la API 
+router = routers.DefaultRouter()
+
+#Asociamos las rutas para las vistas que importamos, User y Group
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #Incluye todas las rutas generadas por router desde la raiz de la app asi: "GET /users/": Lista todos los usuarios.
+    path('', include(router.urls)),
+
+    #Incluye URLs de autenticacion y prefijo para ingresar a la API, añade funcionalidades de login y logout
+    #la url iria api-autenticacion/login-logout
+    path('api-autenticacion/', include('rest_framework.urls', namespace='rest_framework'))
 ]
